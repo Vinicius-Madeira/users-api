37e335ef557bb5d0ae10559371da4ac99fc6d067
Vinicius Madeira <viniciussmadeira@gmail.com>
2024-06-10 01:19:05 -0300

    add: user fields validation error handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,17 +2,20 @@ package controller
 
 import (
 	"fmt"
-	"github.com/Vinicius-Madeira/go-web-app/src/configuration/rest_err"
+	"github.com/Vinicius-Madeira/go-web-app/src/configuration/validation"
 	"github.com/Vinicius-Madeira/go-web-app/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 func CreateUser(c *gin.Context) {
 
+	log.Println("Init createUser controller")
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		log.Printf("Error trying to marshal object, error:%s\n", err.Error())
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -2,7 +2,7 @@ package request
 
 type UserRequest struct {
 	Email    string `json:"email" binding:"required,email"`
-	Password string `json:"password" binding:"required,min=6,containsany=!@#$%*`
+	Password string `json:"password" binding:"required,min=6,containsany=!@#$%*"`
 	Name     string `json:"name" binding:"required,min=4,max=100"`
 	Age      int8   `json:"age" binding:"required,min=1,max=150"`
 }
