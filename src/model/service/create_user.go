023f305322db316ba946a3b5dfda109f8c27ea5f
Vinicius Madeira <viniciussmadeira@gmail.com>
2024-06-15 02:24:58 -0300

    chore: add validation for existing email when creating user

--- src/model/service/create_user.go
+++ src/model/service/create_user.go
@@ -9,16 +9,29 @@ import (
 
 func (uds *userDomainService) CreateUserServices(userDomain model.UserDomainInterface) (model.UserDomainInterface, *rest_err.RestError) {
 
-	logger.Info("Init createUser model", zap.String("journey", "createUser"))
+	logger.Info("Init createUser model",
+		zap.String("journey", "createUser"))
+
+	_, err := uds.FindUserByEmailServices(userDomain.GetEmail())
+	if err == nil {
+		logger.Error("Email already exists on database",
+			err,
+			zap.String("journey", "createUser"))
+		return nil, rest_err.NewBadRequestError("Email is already being used")
+	}
 
 	userDomain.EncryptPassword()
 
 	userDomainRepository, err := uds.userRepository.CreateUser(userDomain)
 	if err != nil {
-		logger.Error("Error trying to call repository", err, zap.String("journey", "createUser"))
+		logger.Error("Error trying to call repository",
+			err,
+			zap.String("journey", "createUser"))
 		return nil, err
 	}
 
-	logger.Info("CreateUser service executed successfully", zap.String("journey", "createUser"), zap.String("userID", userDomainRepository.GetID()))
+	logger.Info("CreateUser service executed successfully",
+		zap.String("journey", "createUser"),
+		zap.String("userID", userDomainRepository.GetID()))
 	return userDomainRepository, nil
 }
